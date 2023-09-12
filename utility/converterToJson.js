3f25ce67bafb08d42a7610e8d4e2b75df2eec3f3
Gaurav10997 <you@example.com>
2023-09-12 12:28:03 +0530

    transfer in laptop

---
+++ utility/converterToJson.js
@@ -0,0 +1,13 @@
+// const args = process.argv.slice(2);
+
+// // Output the remaining arguments as an array
+// console.log(args);
+
+// [
+// private Integer bookId;
+
+// private String bookName ;
+
+// private String authorName ;
+
+// ]
